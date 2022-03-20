981f525c77bb188b9e8da60e90b1c98c8de2edde
vamsichamarthi1147 <86275315+chamarthivamsidev@users.noreply.github.com>
2022-03-20 17:36:16 +0530

    added

---
+++ Binary_Search/first_occurance_of_a_element.js
@@ -0,0 +1,32 @@
+//Problem : First occurance of an element or lower bond.
+
+//Input
+//let arr = [1,3,4,5,5,8,9]
+//let k = 5;
+
+// output : 3
+
+//Note : if element present print index or print -1.
+
+let low = 0;
+let high = n - 1;
+
+console.log(binarySearch(arr, low, high));
+
+function binarySearch(arr, low, high) {
+  let ans = -1;
+
+  while (low <= high) {
+    let mid = low + Math.floor((high - low) / 2);
+    if (arr[mid] == k) {
+      ans = mid;
+      high = mid - 1;
+    } else if (arr[mid] > k) {
+      high = mid - 1;
+    } else low = mid + 1;
+  }
+  return ans;
+}
+
+// Time Complexity --> O(logN)
+// Space Complexity --> O(1)
