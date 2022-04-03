528386cb9d81b34f87734243632667a0e2d4c8bc
vamsichamarthi1147 <86275315+chamarthivamsidev@users.noreply.github.com>
2022-04-03 19:18:46 +0530

    linked_list

--- Linked_List/1.Linked_List_Cycle.js
+++ Linked_List/1.Linked_List_Cycle.js
@@ -1,26 +1,36 @@
 //Givenhead, the head of a linked list, determine if the linked list has a cycle in it.
-// Problem function complete.
+// This is a function complete problem.
+//Refer to sample input for understanding
+//The first line contains the number of nodes (t)
+//Next t lines contains the node of the linked list
+
 //Sample Input
-1;
 3;
 1;
 2;
-3 - 1;
+3;
 
 //Sample output
-false;
+2;
 
-var hasCycle = function (head) {
-  let p2 = head;
+//Approach => first calculate size of linked list with for loop and find middle point.Take a pointer and move the pointer to the middle point.return pointer data.
+
+var middleNode = function (head) {
+  let size = 0;
+
   let p1 = head;
+  let p2 = head;
 
-  while (p2 !== null && p2.next !== null) {
+  while (p1 !== null) {
+    size++;
     p1 = p1.next;
-    p2 = p2.next.next;
-
-    if (p1 === p2) {
-      return true;
-    }
   }
-  return false;
+
+  let pos = Math.floor(size / 2);
+
+  for (let i = 0; i < pos; i++) {
+    p2 = p2.next;
+  }
+
+  return p2.data;
 };
---
+++ Linked_List/2.middle_node.js
@@ -0,0 +1,11 @@
+//Given a non-empty, singly linked list with the head nodehead, return a middle node of the linked list.
+// Problem is function complete.
+//Sample Input
+1;
+3;
+1;
+2;
+3 - 1;
+
+//Sample output
+false;
